467e3f0f4e99b1b906fb4778d87f8d9f7e1df4f8
asiamohammed <mohamedasia021@gmail.com>
2022-09-06 01:58:49 +0300

    rendered html to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
+const popular = document.querySelector('#popular .disp')
+const neww = document.querySelector('#new .disp')
+const local = document.querySelector('#local .disp')
+
 function fetchData(){
       return fetch('http://localhost:3000/cocktails')
       .then(response => response.json())
       .then(json => json)
 }
 
-let data = fetchData()
-
-
 function createHtml(data){
       const div = document.createElement('div')
+      div.className='card'
       const html = `
             <img src="${data.image}" alt="${data.name}">
             <h4>${data.name}</h4>
@@ -26,3 +28,21 @@ function createHtml(data){
       div.appendChild(i)
       return div
 }
+
+fetchData().then(data=>{
+      data.forEach(cocktail => {
+            switch (cocktail.category) {
+                  case 'popular':
+                        popular.appendChild(createHtml(cocktail))
+                        break;
+                  case 'new':
+                        neww.appendChild(createHtml(cocktail))
+                        break;
+                  case 'local':
+                        local.appendChild(createHtml(cocktail))
+                        break;
+                  default:
+                        break;
+            }
+      });
+})
