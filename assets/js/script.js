af85af57c6a988abd9327d1ca2aa88888f9f978e
asiamohammed <mohamedasia021@gmail.com>
2022-09-06 01:48:46 +0300

    create function for appending html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-fetch('http://localhost:3000/cocktails')
-.then(response => response.json())
-.then((data) => {
-      console.log(data);
-});
+function fetchData(){
+      fetch('http://localhost:3000/cocktails')
+      .then(response => response.json())
+      .then(json => json)
+}
+fetchData()
 
+function createHtml(data){
+      const html = `
+      <div class="card">
+            <h2>${data.name}</h2>
+            <img src="${data.image}" alt="${data.name}">
+      </div>
+      `
+      const i = document.createElement('i')
+      i.className='fas fa-heart'
+      i.addEventListener('click', () => {
+            if (i.style.color === 'red') {
+                  i.style.color = 'black'
+            } else {
+                  i.style.color = 'red'
+            }
+      })
+      return html
+}
